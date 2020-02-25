ea34b4af3f47d9e0499b114984dcf6ba6570205e
Ian Martinez <ianmartinez97@live.com>
2020-02-25 11:35:41 -0600

    Intial commit

---
+++ SummerRain/SummerRain/SummerRainView.h
@@ -0,0 +1,13 @@
+//
+//  SummerRainView.h
+//  SummerRain
+//
+//  Created by Ian Martinez on 2/25/20.
+//  Copyright © 2020 Ian Martinez. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SummerRainView : ScreenSaverView
+
+@end
